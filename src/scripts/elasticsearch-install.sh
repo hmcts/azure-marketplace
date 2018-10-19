24f11c7d477330f462a05302a2313246eb9f5b1c
Dwayne Bailey <Dwayne.Bailey@HMCTS.net>
2018-10-19 15:55:13 +0100

    Supply custom DNS search domain

--- src/scripts/elasticsearch-install.sh
+++ src/scripts/elasticsearch-install.sh
@@ -143,7 +143,7 @@ SAML_METADATA_URI=""
 SAML_SP_URI=""
 
 #Loop through options passed
-while getopts :n:m:v:A:R:K:S:F:Z:p:a:k:L:C:B:E:H:G:T:W:V:J:N:D:O:P:xyzldjh optname; do
+while getopts :n:m:v:A:R:K:S:F:Z:p:a:k:L:C:B:E:H:G:T:W:V:J:N:D:O:P:X:xyzldjh optname; do
   log "Option $optname set"
   case $optname in
     n) #set cluster name
@@ -241,6 +241,9 @@ while getopts :n:m:v:A:R:K:S:F:Z:p:a:k:L:C:B:E:H:G:T:W:V:J:N:D:O:P:xyzldjh optna
       ;;
     E) #azure storage account endpoint suffix
       STORAGE_SUFFIX="${OPTARG}"
+      ;;
+    X) #DNS custom search domain
+      DOMAIN_SEARCH="${OPTARG}"
       ;;
     h) #show help
       help
@@ -1108,6 +1111,7 @@ configure_os_properties()
     log "[configure_os_properties] configuring operating system level configuration"
     # DNS Retry
     echo "options timeout:10 attempts:5" >> /etc/resolvconf/resolv.conf.d/head
+    echo "search $DOMAIN_SEARCH" >> /etc/resolvconf/resolv.conf.d/base
     resolvconf -u
 
     # Required for bootstrap memory lock with systemd
