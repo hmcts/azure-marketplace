f8ece72cd1ed62baadc2b40d8ca524d8d766428c
Constantin Preda <constantin.preda@hmcts.net>
2020-05-26 14:42:49 +0100

    adding "securityReadPassword" feature to template

--- build/tasks/arm-validator.js
+++ build/tasks/arm-validator.js
@@ -98,6 +98,7 @@ var bootstrapTest = (t, defaultVersion) =>
   testParameters.sshPublicKey.value = config.deployments.ssh;
   testParameters.securityBootstrapPassword.value = config.deployments.securityPassword;
   testParameters.securityAdminPassword.value = config.deployments.securityPassword;
+  testParameters.securityReadPassword.value = config.deployments.securityPassword;
   testParameters.securityRemoteMonitoringPassword.value = config.deployments.securityPassword;
   testParameters.securityKibanaPassword.value = config.deployments.securityPassword;
   testParameters.securityLogstashPassword.value = config.deployments.securityPassword;
